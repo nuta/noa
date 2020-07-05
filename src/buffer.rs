b75f8689fd548305471579746ddac98456fcdc3b
Seiya Nuta <nuta@seiya.me>
2020-07-05 13:14:16 +0000

    Update

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-use crate::gap_buffer::GapBuffer;
+use ropey::Rope;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Point {
@@ -13,6 +13,10 @@ impl Point {
             line,
             col,
         }
+    }
+
+    pub fn index_in_rope(&self, rope: &Rope) -> usize {
+        rope.line_to_char(self.line) + self.col
     }
 }
 
@@ -50,7 +54,7 @@ impl fmt::Display for Range {
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Cursor {
     Normal(Point),
     Selection(Range),
@@ -66,25 +70,63 @@ impl CursorSet {
             cursors: vec![Cursor::Normal(Point::new(0, 0))],
         }
     }
+
+    pub fn cursors(&self) -> &[Cursor] {
+        &self.cursors
+    }
+
+    pub fn move_by_insertion(&mut self, string: &str) -> Vec<Cursor> {
+        let y_diff = string.matches('\n').count();
+        let x_diff = string.rfind('\n').unwrap_or(string.len());
+
+        let old_cursors = self.cursors.clone();
+        for cursor in &mut self.cursors {
+            let new_pos = match cursor {
+                Cursor::Normal(Point { line, col }) => {
+                    Point::new(*line + y_diff, *col + x_diff)
+                }
+                Cursor::Selection(Range { start, .. }) => {
+                    Point::new(start.line + y_diff, start.col + x_diff)
+                }
+            };
+
+            // TODO: Adjust other cursors.
+            *cursor = Cursor::Normal(new_pos);
+        }
+
+        old_cursors
+    }
 }
 
 pub struct Buffer {
-    gb: GapBuffer,
+    buf: Rope,
     cursors: CursorSet,
 }
 
 impl Buffer {
     pub fn new() -> Buffer {
         Buffer {
-            gb: GapBuffer::new(),
+            buf: Rope::new(),
             cursors: CursorSet::new(),
         }
     }
 
-    pub fn insert_range(string: &str, range: &Range) {
+    pub fn text(&self) -> String {
+        self.buf.to_string()
     }
 
-    pub fn delete_range(range: &Range) {
+    pub fn insert(&mut self, string: &str) {
+        for cursor in self.cursors.move_by_insertion(&string) {
+            match cursor {
+                Cursor::Normal(pos) => {
+                    self.insert_range(string, &pos);
+                }
+                Cursor::Selection(range) => {
+                    self.delete_range(&range);
+                    self.insert_range(string, &range.start);
+                }
+            };
+        }
     }
 
     pub fn begin_undo_group(&mut self) {
@@ -98,6 +140,16 @@ impl Buffer {
 
     pub fn redo(&mut self) {
     }
+
+    fn insert_range(&mut self, string: &str, pos: &Point) {
+        self.buf.insert(pos.index_in_rope(&self.buf), string);
+    }
+
+    fn delete_range(&mut self, range: &Range) {
+        let start = range.start.index_in_rope(&self.buf);
+        let end = range.end.index_in_rope(&self.buf);
+        self.buf.remove(start..end);
+    }
 }
 
 #[cfg(test)]
@@ -107,5 +159,8 @@ mod test {
     #[test]
     fn single_cursor() {
         let mut b = Buffer::new();
+        b.insert("Hello");
+        b.insert(" World!");
+        assert_eq!(b.text(), "Hello World!");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 #![allow(unused)]
-#![feature(test)]
-extern crate test;
 
 mod buffer;
-mod gap_buffer;
 
 fn main() {
     println!("Hello, world!");
