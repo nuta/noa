af78129d02a7e16b2b1615f6e437040d19a5ff5e
Seiya Nuta <nuta@seiya.me>
2020-09-20 17:03:40 +0900

    Update

--- src/search.rs
+++ src/search.rs
@@ -1,10 +1,10 @@
-use std::path::Path;
+use crate::command_box::{File, Location};
+use crate::rope::{Cursor, Point, Range};
 use grep::matcher::Matcher;
 use grep::regex::{RegexMatcher, RegexMatcherBuilder};
 use grep::searcher::{Searcher, SearcherBuilder, Sink, SinkMatch};
 use ignore::WalkBuilder;
-use crate::command_box::{Location, File};
-use crate::rope::{Cursor, Range, Point};
+use std::path::Path;
 
 struct GrepSink<'a> {
     path: &'a Path,
@@ -13,7 +13,11 @@ struct GrepSink<'a> {
 }
 
 impl<'a> GrepSink<'a> {
-    pub fn new(path: &'a Path, matcher: &'a RegexMatcher, locations: &'a mut Vec<Location>) -> GrepSink<'a> {
+    pub fn new(
+        path: &'a Path,
+        matcher: &'a RegexMatcher,
+        locations: &'a mut Vec<Location>,
+    ) -> GrepSink<'a> {
         GrepSink {
             path,
             matcher,
@@ -24,11 +28,7 @@ impl<'a> GrepSink<'a> {
 
 impl<'a> Sink for GrepSink<'a> {
     type Error = std::io::Error;
-    fn matched(
-        &mut self,
-        _searcher: &Searcher,
-        sm: &SinkMatch,
-    ) -> Result<bool, Self::Error> {
+    fn matched(&mut self, _searcher: &Searcher, sm: &SinkMatch) -> Result<bool, Self::Error> {
         let text = std::str::from_utf8(sm.bytes()).unwrap();
         let m = self.matcher.find(text.as_bytes())?.unwrap();
         let matched_text = &text[m.start()..m.end()];
@@ -37,7 +37,8 @@ impl<'a> Sink for GrepSink<'a> {
         let start_y = sm.line_number().unwrap() as usize;
         let start_x = text[..m.start()].chars().count();
 
-        let end_x = matched_text.rfind('\n')
+        let end_x = matched_text
+            .rfind('\n')
             .map(|x| matched_text_count - x - 1)
             .unwrap_or_else(|| matched_text_count);
         let end_y = start_y + matched_text.matches('\n').count();
@@ -45,7 +46,10 @@ impl<'a> Sink for GrepSink<'a> {
         let range = Range::new(start_y, start_x, end_y, end_x);
 
         let display_name = self.path.to_str().unwrap().to_owned();
-        let file = File { display_name, path: self.path.to_owned() };
+        let file = File {
+            display_name,
+            path: self.path.to_owned(),
+        };
         self.locations.push(Location { file, range });
         Ok(true)
     }
@@ -82,7 +86,10 @@ pub fn list_files(dir: &Path, pat: &str) -> Vec<File> {
             let display_name = pathbuf.to_str().unwrap().to_owned();
             // TODO: fuzzy match
             if display_name.contains(pat) {
-                files.push(File { display_name, path: pathbuf });
+                files.push(File {
+                    display_name,
+                    path: pathbuf,
+                });
             }
         }
     }
