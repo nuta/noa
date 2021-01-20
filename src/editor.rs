0b91e1b3cc7fbc2494e45a640ebc7eda0b113ed1
Seiya Nuta <nuta@seiya.me>
2021-01-20 20:10:24 +0900

    Update

--- src/editor.rs
+++ src/editor.rs
@@ -1,11 +1,31 @@
-pub struct Editor {}
+use crate::terminal::{KeyEvent, Terminal};
+use std::sync::mpsc::{channel, Receiver};
+
+pub enum Event {
+    Key(KeyEvent),
+    KeyBatch(String),
+    Resize { rows: usize, cols: usize },
+}
+
+enum EditorMode {
+    Normal,
+}
+
+pub struct Editor {
+    mode: EditorMode,
+    terminal: Terminal,
+    event_queue: Receiver<Event>,
+}
 
 impl Editor {
     pub fn new() -> Editor {
-        Editor {}
+        let (tx, rx) = channel();
+        Editor {
+            mode: EditorMode::Normal,
+            terminal: Terminal::new(tx),
+            event_queue: rx,
+        }
     }
 
-    pub fn run(&mut self) {
-        panic!("oops");
-    }
+    pub fn run(&mut self) {}
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,3 +1,6 @@
+use crate::buffer::{Buffer, TopLeft};
+use crate::editor::Event;
+use crate::rope::Point;
 use crossterm::event::{
     self, DisableMouseCapture, EnableMouseCapture, Event as TermEvent, MouseButton,
 };
@@ -8,6 +11,7 @@ use crossterm::terminal::{
 use crossterm::{execute, queue};
 use std::cmp::{max, min};
 use std::io::{stdout, Write};
+use std::sync::mpsc::Sender;
 use std::thread;
 use std::time::Duration;
 use unicode_width::UnicodeWidthChar;
@@ -41,115 +45,103 @@ fn num_of_digits(mut n: usize) -> usize {
     }
 }
 
-#[derive(Debug)]
-pub enum MouseEvent {
-    ClickText {
-        /// The position in the buffer. They could be larger than the line lengths.
-        pos: Point,
-        alt: bool,
-    },
-    ClickLineMap {
-        y: usize,
-    },
-    ScrollUp,
-    ScrollDown,
-    Drag {
-        /// The position in the buffer. They could be larger than the line lengths.
-        pos: Point,
-    },
+pub fn handle_term_event(event_queue: Sender<Event>) {
+    fn handle_event(event_queue: &Sender<Event>, ev: TermEvent) {
+        match ev {
+            TermEvent::Key(key) => {
+                event_queue.send(Event::Key(key)).ok();
+            }
+            TermEvent::Mouse(mouse) => {
+                // event_queue.enqueue(Event::Mouse(mouse));
+            }
+            TermEvent::Resize(cols, rows) => {
+                event_queue
+                    .send(Event::Resize {
+                        cols: cols as usize,
+                        rows: rows as usize,
+                    })
+                    .ok();
+            }
+        }
+    }
+
+    fn is_next_available() -> crossterm::Result<bool> {
+        event::poll(Duration::from_secs(0))
+    }
+
+    loop {
+        if let Ok(ev) = event::read() {
+            match ev {
+                TermEvent::Key(KeyEvent {
+                    code: KeyCode::Char(key),
+                    modifiers: KeyModifiers::NONE,
+                }) if is_next_available().unwrap() => {
+                    let mut next_event = None;
+                    let mut buf = key.to_string();
+                    while is_next_available().unwrap() && next_event.is_none() {
+                        match event::read() {
+                            Ok(TermEvent::Key(KeyEvent {
+                                code: KeyCode::Char(ch),
+                                modifiers: KeyModifiers::SHIFT,
+                            })) => {
+                                buf.push(ch);
+                            }
+                            Ok(TermEvent::Key(KeyEvent {
+                                code,
+                                modifiers: KeyModifiers::NONE,
+                            })) => match code {
+                                KeyCode::Char(ch) => {
+                                    buf.push(ch);
+                                }
+                                KeyCode::Enter => {
+                                    buf.push('\n');
+                                }
+                                KeyCode::Tab => {
+                                    buf.push('\t');
+                                }
+                                _ => {
+                                    next_event = Some(ev);
+                                }
+                            },
+                            Ok(ev) => {
+                                next_event = Some(ev);
+                            }
+                            _ => {}
+                        }
+                    }
+
+                    event_queue.send(Event::KeyBatch(buf)).ok();
+                    if let Some(ev) = next_event {
+                        handle_event(&event_queue, ev);
+                    }
+                }
+                _ => {
+                    handle_event(&event_queue, ev);
+                }
+            }
+        }
+    }
 }
 
 pub struct Terminal {
-    rows: usize,
-    cols: usize,
-    current_top_left: TopLeft,
-    current_num_lines: usize,
-    text_start_x: usize,
-    text_end_x: usize,
-    text_height: usize,
-}
-
+        rows: usize,
+        cols: usize,
+        current_top_left: TopLeft,
+        current_num_lines: usize,
+        text_start_x: usize,
+        text_end_x: usize,
+        text_height: usize,
+    }
+    
 impl Terminal {
-    pub fn new(event_queue: EventQueue) -> Terminal {
+    pub fn new(event_queue: Sender<Event>) -> Terminal {
         let (cols, rows) = size().expect("failed to get the terminal size");
         enable_raw_mode().expect("failed to enable the raw mode");
         execute!(stdout(), EnableMouseCapture).expect("failed to enable mouse capture");
         execute!(stdout(), EnterAlternateScreen).expect("failed to enter the alternative screen");
 
         thread::spawn(move || {
-            fn handle_event(event_queue: &EventQueue, ev: TermEvent) {
-                match ev {
-                    TermEvent::Key(key) => {
-                        event_queue.enqueue(Event::Key(key));
-                    }
-                    TermEvent::Mouse(mouse) => {
-                        event_queue.enqueue(Event::Mouse(mouse));
-                    }
-                    TermEvent::Resize(cols, rows) => {
-                        event_queue.enqueue(Event::Resize {
-                            cols: cols as usize,
-                            rows: rows as usize,
-                        });
-                    }
-                }
-            }
-
-            fn is_next_available() -> crossterm::Result<bool> {
-                event::poll(Duration::from_secs(0))
-            }
-
-            loop {
-                if let Ok(ev) = event::read() {
-                    match ev {
-                        TermEvent::Key(KeyEvent {
-                            code: KeyCode::Char(key),
-                            modifiers: KeyModifiers::NONE,
-                        }) if is_next_available().unwrap() => {
-                            let mut next_event = None;
-                            let mut buf = key.to_string();
-                            while is_next_available().unwrap() && next_event.is_none() {
-                                match event::read() {
-                                    Ok(TermEvent::Key(KeyEvent {
-                                        code: KeyCode::Char(ch),
-                                        modifiers: KeyModifiers::SHIFT,
-                                    })) => {
-                                        buf.push(ch);
-                                    }
-                                    Ok(TermEvent::Key(KeyEvent {
-                                        code,
-                                        modifiers: KeyModifiers::NONE,
-                                    })) => match code {
-                                        KeyCode::Char(ch) => {
-                                            buf.push(ch);
-                                        }
-                                        KeyCode::Enter => {
-                                            buf.push('\n');
-                                        }
-                                        KeyCode::Tab => {
-                                            buf.push('\t');
-                                        }
-                                        _ => {
-                                            next_event = Some(ev);
-                                        }
-                                    },
-                                    Ok(ev) => {
-                                        next_event = Some(ev);
-                                    }
-                                    _ => {}
-                                }
-                            }
-
-                            event_queue.enqueue(Event::KeyBatch(buf));
-                            if let Some(ev) = next_event {
-                                handle_event(&event_queue, ev);
-                            }
-                        }
-                        _ => {
-                            handle_event(&event_queue, ev);
-                        }
-                    }
-                }
-            }
+            handle_term_event(event_queue);
         });
 
         Terminal {
