0dd39c6bf6760583948b5f9137b4d28b376bc32d
Seiya Nuta <nuta@seiya.me>
2021-05-04 16:46:25 +0000

    Update

--- src/terminal.rs
+++ src/terminal.rs
@@ -81,92 +81,92 @@ impl DisplayWidth for usize {
     }
 }
 
+async fn terminal_input_handler(event_queue: EventQueue) {
+    let mut stream = EventStream::new().fuse();
+
+    fn handle_event(event_queue: &EventQueue, ev: TermEvent) {
+        match ev {
+            TermEvent::Key(key) => {
+                event_queue.enqueue(Event::Key(key));
+            }
+            TermEvent::Mouse(_) => {
+                unreachable!();
+            }
+            TermEvent::Resize(cols, rows) => {
+                event_queue.enqueue(Event::Resize {
+                    screen_width: cols as usize,
+                    screen_height: rows as usize,
+                });
+            }
+        }
+    }
+
+    fn is_next_available() -> crossterm::Result<bool> {
+        crossterm::event::poll(Duration::from_secs(0))
+    }
+
+    loop {
+        if let Some(Ok(ev)) = stream.next().await {
+            match ev {
+                TermEvent::Key(KeyEvent {
+                    code: KeyCode::Char(key),
+                    modifiers: KeyModifiers::NONE,
+                }) if is_next_available().unwrap() => {
+                    let mut next_event = None;
+                    let mut buf = key.to_string();
+                    while is_next_available().unwrap() && next_event.is_none() {
+                        if let Some(Ok(ev)) = stream.next().await {
+                            match ev {
+                                TermEvent::Key(KeyEvent {
+                                    code: KeyCode::Char(ch),
+                                    modifiers: KeyModifiers::SHIFT,
+                                }) => {
+                                    buf.push(ch);
+                                }
+                                TermEvent::Key(KeyEvent {
+                                    code,
+                                    modifiers: KeyModifiers::NONE,
+                                }) => match code {
+                                    KeyCode::Char(ch) => {
+                                        buf.push(ch);
+                                    }
+                                    KeyCode::Enter => {
+                                        buf.push('\n');
+                                    }
+                                    KeyCode::Tab => {
+                                        buf.push('\t');
+                                    }
+                                    _ => {
+                                        next_event = Some(ev);
+                                    }
+                                },
+                                ev => {
+                                    next_event = Some(ev);
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
+
+                    event_queue.enqueue(Event::KeyBatch(buf));
+                    if let Some(ev) = next_event {
+                        handle_event(&event_queue, ev);
+                    }
+                }
+                _ => {
+                    handle_event(&event_queue, ev);
+                }
+            }
+        }
+    }
+}
+
 impl Terminal {
     pub fn new(event_queue: EventQueue) -> Terminal {
-        let (cols, rows) = size().expect("failed to get the terminal size");
         enable_raw_mode().expect("failed to enable the raw mode");
         execute!(stdout(), EnterAlternateScreen).expect("failed to enter the alternative screen");
-
-        tokio::spawn(async move {
-            let mut stream = EventStream::new().fuse();
-
-            fn handle_event(event_queue: &EventQueue, ev: TermEvent) {
-                match ev {
-                    TermEvent::Key(key) => {
-                        event_queue.enqueue(Event::Key(key));
-                    }
-                    TermEvent::Mouse(_) => {
-                        unreachable!();
-                    }
-                    TermEvent::Resize(cols, rows) => {
-                        event_queue.enqueue(Event::Resize {
-                            screen_width: cols as usize,
-                            screen_height: rows as usize,
-                        });
-                    }
-                }
-            }
-
-            fn is_next_available() -> crossterm::Result<bool> {
-                crossterm::event::poll(Duration::from_secs(0))
-            }
-
-            loop {
-                if let Some(Ok(ev)) = stream.next().await {
-                    match ev {
-                        TermEvent::Key(KeyEvent {
-                            code: KeyCode::Char(key),
-                            modifiers: KeyModifiers::NONE,
-                        }) if is_next_available().unwrap() => {
-                            let mut next_event = None;
-                            let mut buf = key.to_string();
-                            while is_next_available().unwrap() && next_event.is_none() {
-                                if let Some(Ok(ev)) = stream.next().await {
-                                    match ev {
-                                        TermEvent::Key(KeyEvent {
-                                            code: KeyCode::Char(ch),
-                                            modifiers: KeyModifiers::SHIFT,
-                                        }) => {
-                                            buf.push(ch);
-                                        }
-                                        TermEvent::Key(KeyEvent {
-                                            code,
-                                            modifiers: KeyModifiers::NONE,
-                                        }) => match code {
-                                            KeyCode::Char(ch) => {
-                                                buf.push(ch);
-                                            }
-                                            KeyCode::Enter => {
-                                                buf.push('\n');
-                                            }
-                                            KeyCode::Tab => {
-                                                buf.push('\t');
-                                            }
-                                            _ => {
-                                                next_event = Some(ev);
-                                            }
-                                        },
-                                        ev => {
-                                            next_event = Some(ev);
-                                        }
-                                        _ => {}
-                                    }
-                                }
-                            }
-
-                            event_queue.enqueue(Event::KeyBatch(buf));
-                            if let Some(ev) = next_event {
-                                handle_event(&event_queue, ev);
-                            }
-                        }
-                        _ => {
-                            handle_event(&event_queue, ev);
-                        }
-                    }
-                }
-            }
-        });
-
+        tokio::spawn(terminal_input_handler(event_queue));
+        let (cols, rows) = size().expect("failed to get the terminal size");
         Terminal {
             screen_height: rows as usize,
             screen_width: cols as usize,
