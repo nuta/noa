4156250b0597191953929decd77d291c1c9dbd9b
Seiya Nuta <nuta@seiya.me>
2021-05-29 12:14:49 +0000

    Update

---
+++ src/lspd/main.rs
@@ -0,0 +1,42 @@
+#[macro_use]
+extern crate log;
+
+use dirs::home_dir;
+use log::LevelFilter;
+use simplelog::{CombinedLogger, Config, TermLogger, TerminalMode, WriteLogger};
+use std::fs::OpenOptions;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Opt {}
+
+#[tokio::main]
+async fn main() {
+    CombinedLogger::init(vec![
+        TermLogger::new(
+            LevelFilter::Trace,
+            Config::default(),
+            TerminalMode::Mixed,
+            simplelog::ColorChoice::Auto,
+        ),
+        WriteLogger::new(
+            LevelFilter::Trace,
+            Config::default(),
+            OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open(home_dir().unwrap().join(".noa-lspd.log"))
+                .unwrap(),
+        ),
+    ])
+    .unwrap();
+
+    std::panic::set_hook(Box::new(|info| {
+        error!("{}", info);
+        error!("{:#?}", backtrace::Backtrace::new());
+    }));
+
+    trace!("starting");
+
+    let opt = Opt::from_args();
+}
