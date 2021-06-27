d85d817353d5e02d872a29af755c8dd8ecc484c5
Seiya Nuta <nuta@seiya.me>
2021-06-28 01:21:29 +0900

    Update

--- src/noa/open.rs
+++ src/noa/open.rs
@@ -1,14 +1,11 @@
 use std::{
-    io::{BufReader, Read},
     path::{Path, PathBuf},
     process::{Command, Stdio},
 };
 
 use noa_buffer::Point;
-use tokio::io::split;
 
 pub fn open_path_in_tmux(pane: &str, mouse_y: usize, mouse_x: usize) {
-    println!("loading tmux...");
     let output = Command::new("tmux")
         .args(&[
             "capture-pane",
@@ -30,8 +27,6 @@ pub fn open_path_in_tmux(pane: &str, mouse_y: usize, mouse_x: usize) {
     if let Some((path, point)) = extract_path_and_point(&output.stdout, mouse_y, mouse_x) {
         println!("opening {:?} {:?}", path, point);
     }
-
-    // tooooooooooooooooooooooooooooooooooooooooooooo_lloooooooooooooooooooooooooooooooooooooooooooong_caaaaaaaaaaaaaaaaaaaaaaaaaaaat
 }
 
 fn is_valid_path_char(byte: u8) -> bool {
@@ -44,64 +39,67 @@ fn is_valid_path_char(byte: u8) -> bool {
 }
 
 fn extract_path_and_point(
-    mut heystack: &[u8],
+    heystack: &[u8],
     mouse_y: usize,
     mouse_x: usize,
 ) -> Option<(PathBuf, Point)> {
     let mut skip = mouse_y;
+    let mut cursor_i = None;
     for (i, &byte) in heystack.iter().enumerate() {
         if skip == 0 {
-            let mut start = i + mouse_x;
-            while start > 0 {
-                match heystack.get(start) {
-                    Some(&byte) if byte.is_ascii_control() => {}
-                    Some(&byte) if is_valid_path_char(byte) => {}
-                    _ => break,
-                }
-
-                start -= 1;
-            }
-
-            let mut end = i + mouse_x;
-            loop {
-                match heystack.get(end) {
-                    Some(&byte) if byte.is_ascii_control() => {}
-                    Some(&byte) if is_valid_path_char(byte) => {}
-                    _ => break,
-                }
-
-                end += 1;
-            }
-
-            let matched_bytes = &heystack[start..end];
-            let mut matched_substr = std::str::from_utf8(matched_bytes)
-                .expect("non-utf8 matched text")
-                .to_string();
-            matched_substr.retain(|c| !"\n\r".contains(c));
-
-            let mut words = matched_substr.split(':');
-            let path = words.next().map(|s| s.trim());
-            let lineno = words.next().map(|s| s.trim());
-            let colno = words.next().map(|s| s.trim());
-            match (path, lineno, colno) {
-                (Some(path), Some(lineno), Some(colno)) => {
-                    match (Path::new(path), lineno.parse::<usize>(), colno.parse()) {
-                        (path, Ok(lineno), Ok(colno)) if path.exists() && lineno > 0 => {
-                            return Some((path.to_owned(), Point::new(lineno - 1, colno)));
-                        }
-                        _ => {}
-                    }
-                }
-                _ => {}
-            }
-
-            println!("matched_substr: '{}'", matched_substr);
-            return None;
+            cursor_i = Some(i);
+            break;
         }
 
         if byte == b'\n' {
             skip -= 1;
         }
+    }
+
+    let cursor_i = cursor_i.expect("invalid mouse cursor position");
+
+    let mut start = cursor_i + mouse_x;
+    while start > 0 {
+        match heystack.get(start) {
+            Some(&byte) if byte.is_ascii_control() => {}
+            Some(&byte) if is_valid_path_char(byte) => {}
+            _ => break,
+        }
+
+        start -= 1;
+    }
+
+    let mut end = cursor_i + mouse_x;
+    loop {
+        match heystack.get(end) {
+            Some(&byte) if byte.is_ascii_control() => {}
+            Some(&byte) if is_valid_path_char(byte) => {}
+            _ => break,
+        }
+
+        end += 1;
+    }
+
+    let matched_bytes = &heystack[start..end];
+    let mut matched_substr = std::str::from_utf8(matched_bytes)
+        .expect("non-utf8 matched text")
+        .to_string();
+    matched_substr.retain(|c| !"\n\r".contains(c));
+
+    let mut words = matched_substr.split(':');
+    let path = words.next().map(|s| s.trim());
+    let lineno = words.next().map(|s| s.trim());
+    let colno = words.next().map(|s| s.trim());
+    match (path, lineno, colno) {
+        (Some(path), Some(lineno), Some(colno)) => {
+            match (Path::new(path), lineno.parse::<usize>(), colno.parse()) {
+                (path, Ok(lineno), Ok(colno)) if path.exists() && lineno > 0 => {
+                    return Some((path.to_owned(), Point::new(lineno - 1, colno)));
+                }
+                _ => {}
+            }
+        }
+        _ => {}
     }
 
     None
