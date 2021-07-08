eea3c496a1c06f6147dc4703edd41c4e57bde383
Seiya Nuta <nuta@seiya.me>
2021-07-08 21:18:02 +0900

    Update

--- src/replace/main.rs
+++ src/replace/main.rs
@@ -6,6 +6,7 @@ mod search_query;
 mod ui;
 
 use noa_common::logger::install_logger;
+use regex::Regex;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -26,9 +27,42 @@ async fn main() {
     let ui = Ui::new();
     let base_dir = opt.dir.unwrap_or_else(|| std::env::current_dir().unwrap());
 
+    use grep::matcher::Matcher;
+    use grep::regex::RegexMatcher;
+    use grep::searcher::sinks::UTF8;
+    use grep::searcher::Searcher;
+
+    let query = "oops";
+
+    let matcher = RegexMatcher::new(query).unwrap();
+
     let path_scanner = PathScanner::new(&base_dir);
+
     path_scanner.scan(Box::new(|path: PathBuf| {
-        println!("{}", path.display());
+        // println!("{}", path.display());
+
+        Searcher::new()
+            .search_path(
+                &matcher,
+                &path,
+                UTF8(|lineno, line| {
+                    let m = matcher.find(line.as_bytes())?.unwrap();
+                    let before_text = &line[..m.start()];
+                    let matched_text = &line[m.start()..m.end()];
+                    let after_text = &line[m.end()..];
+                    println!(
+                        "{}:{}: {}\x1b[1;31m{}\x1b[0m{}",
+                        path.display(),
+                        lineno,
+                        before_text,
+                        matched_text,
+                        after_text
+                    );
+                    Ok(true)
+                }),
+            )
+            .unwrap();
+
         true
     }));
 }
