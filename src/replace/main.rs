9f84f7bb1a3ece27332f6788987c0ad2c850bb0b
Seiya Nuta <nuta@seiya.me>
2021-07-08 20:10:26 +0900

    Update

--- src/replace/main.rs
+++ src/replace/main.rs
@@ -1,14 +1,18 @@
 #[macro_use]
 extern crate log;
 
+mod ui;
+
 use noa_common::logger::install_logger;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
+use crate::ui::Ui;
+
 #[derive(StructOpt)]
 struct Opt {
     #[structopt(long, parse(from_os_str))]
-    dir: PathBuf,
+    dir: Option<PathBuf>,
 }
 
 #[tokio::main]
@@ -17,4 +21,21 @@ async fn main() {
     trace!("starting");
 
     let opt = Opt::from_args();
+    let ui = Ui::new();
+
+    use ignore::{WalkBuilder, WalkState};
+    let base_dir = opt.dir.unwrap_or_else(|| std::env::current_dir().unwrap());
+    WalkBuilder::new(base_dir).build_parallel().run(|| {
+        Box::new(|dirent| {
+            if let Ok(dirent) = dirent {
+                let meta = dirent.metadata().unwrap();
+                if !meta.is_file() {
+                    return WalkState::Continue;
+                }
+
+                println!("{}", dirent.path().display());
+            }
+            WalkState::Continue
+        })
+    });
 }
---
+++ src/replace/ui.rs
@@ -0,0 +1,7 @@
+pub struct Ui {}
+
+impl Ui {
+    pub fn new() -> Ui {
+        Ui {}
+    }
+}
