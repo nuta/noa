9e2e73d2d5f5e916532d63f4c935bb751977b81d
Seiya Nuta <nuta@seiya.me>
2022-01-08 23:23:25 +0900

    Update

---
+++ src/buffer/benches/bench_grapheme_iter.rs
@@ -0,0 +1,32 @@
+#![feature(test)]
+
+extern crate test;
+
+use noa_buffer::cursor::*;
+use noa_buffer::raw_buffer::*;
+
+#[bench]
+fn bench_iterate_next_128_graphemes(b: &mut test::Bencher) {
+    // A grapheme ("u" with some marks), consists of U+0075 U+0308 U+0304.
+    let buffer = RawBuffer::from_text(&"\u{0075}\u{0308}\u{0304}".repeat(128));
+
+    b.iter(|| {
+        let mut iter = buffer.grapheme_iter(Position::new(0, 0));
+        while let Some(grapheme) = iter.next() {
+            test::black_box(grapheme);
+        }
+    });
+}
+
+#[bench]
+fn bench_iterate_next_prev_graphemes(b: &mut test::Bencher) {
+    // A grapheme ("u" with some marks), consists of U+0075 U+0308 U+0304.
+    let buffer = RawBuffer::from_text(&"\u{0075}\u{0308}\u{0304}".repeat(128));
+
+    b.iter(|| {
+        let mut iter = buffer.grapheme_iter(Position::new(0, 3 * 128));
+        while let Some(grapheme) = iter.prev() {
+            test::black_box(grapheme);
+        }
+    });
+}
--- src/buffer/lib.rs
+++ src/buffer/lib.rs
@@ -1,4 +1,4 @@
 pub mod buffer;
 pub mod cursor;
 pub mod display_width;
-mod raw_buffer;
+pub mod raw_buffer;
