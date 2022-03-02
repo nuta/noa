f451fa87ca4f3d6891d6180e93304e1101e395ab
Seiya Nuta <nuta@seiya.me>
2022-03-02 16:46:08 +0000

    Update

--- .github/pull_request_template.md
+++ .github/pull_request_template.md
@@ -12,10 +12,6 @@
 
 **Please explain changes in this PR here.**
 
-### Contribution Acceptance Policy
-
-Since this software is written and maintained just for my personal use and is crucial for my daily job, I accept only bug fixes and minor improvements; don't accept suggestions, support requests, and new features that I won't use.
-
 ### Pre-Submission Checklist
 
 When you submit a PR, please make sure your PR satisfies the following checklist:
@@ -25,6 +21,6 @@ When you submit a PR, please make sure your PR satisfies the following checklist
 - [ ] The PR title is well written within 70 characters.
 - [ ] The PR description describes the reason why we need the change.
 - [ ] This is an isolated change. No multiple changes and no unrelated changes are included.
-- [ ] Added tests for the chage.
+- [ ] Added tests for this chage.
 - [ ] Fixed all `cargo clippy` warnings.
 - [ ] Applied `rustfmt`.
--- src/languages/tree_sitter/mod.rs
+++ src/languages/tree_sitter/mod.rs
@@ -2,12 +2,36 @@ pub use tree_sitter::*;
 extern "C" {
     fn tree_sitter_rust() -> Language;
     fn tree_sitter_c() -> Language;
+    fn tree_sitter_javascript() -> Language;
+    fn tree_sitter_go() -> Language;
+    fn tree_sitter_css() -> Language;
+    fn tree_sitter_scss() -> Language;
+    fn tree_sitter_typescript() -> Language;
+    fn tree_sitter_tsx() -> Language;
+    fn tree_sitter_toml() -> Language;
+    fn tree_sitter_json() -> Language;
+    fn tree_sitter_make() -> Language;
+    fn tree_sitter_dockerfile() -> Language;
+    fn tree_sitter_regex() -> Language;
+    fn tree_sitter_comment() -> Language;
 }
 
 pub fn get_tree_sitter_parser(name: &str) -> Option<Language> {
    match name {
         "rust" => Some(unsafe { tree_sitter_rust() }),
         "c" => Some(unsafe { tree_sitter_c() }),
+        "javascript" => Some(unsafe { tree_sitter_javascript() }),
+        "go" => Some(unsafe { tree_sitter_go() }),
+        "css" => Some(unsafe { tree_sitter_css() }),
+        "scss" => Some(unsafe { tree_sitter_scss() }),
+        "typescript" => Some(unsafe { tree_sitter_typescript() }),
+        "tsx" => Some(unsafe { tree_sitter_tsx() }),
+        "toml" => Some(unsafe { tree_sitter_toml() }),
+        "json" => Some(unsafe { tree_sitter_json() }),
+        "make" => Some(unsafe { tree_sitter_make() }),
+        "dockerfile" => Some(unsafe { tree_sitter_dockerfile() }),
+        "regex" => Some(unsafe { tree_sitter_regex() }),
+        "comment" => Some(unsafe { tree_sitter_comment() }),
     _ => None
     }
 }
@@ -16,6 +40,18 @@ pub fn get_highlights_query(name: &str) -> Option<&str> {
    match name {
         "rust" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/rust/highlights.scm")),
         "c" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/c/highlights.scm")),
+        "javascript" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/javascript/highlights.scm")),
+        "go" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/go/highlights.scm")),
+        "css" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/css/highlights.scm")),
+        "scss" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/scss/highlights.scm")),
+        "typescript" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/typescript/highlights.scm")),
+        "tsx" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/tsx/highlights.scm")),
+        "toml" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/toml/highlights.scm")),
+        "json" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/json/highlights.scm")),
+        "make" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/make/highlights.scm")),
+        "dockerfile" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/dockerfile/highlights.scm")),
+        "regex" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/regex/highlights.scm")),
+        "comment" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/comment/highlights.scm")),
     _ => None
     }
 }
@@ -23,6 +59,14 @@ pub fn get_indents_query(name: &str) -> Option<&str> {
    match name {
         "rust" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/rust/indents.scm")),
         "c" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/c/indents.scm")),
+        "javascript" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/javascript/indents.scm")),
+        "go" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/go/indents.scm")),
+        "css" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/css/indents.scm")),
+        "scss" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/scss/indents.scm")),
+        "typescript" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/typescript/indents.scm")),
+        "tsx" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/tsx/indents.scm")),
+        "toml" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/toml/indents.scm")),
+        "json" => Some(include_str!("../tree_sitter/nvim_treesitter/queries/json/indents.scm")),
     _ => None
     }
 }
