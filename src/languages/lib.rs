edb48ba4970c96d2009ff48175eba98b7bbcc530
Seiya Nuta <nuta@seiya.me>
2022-04-02 01:44:41 +0000

    Update

--- src/languages/lib.rs
+++ src/languages/lib.rs
@@ -2,24 +2,44 @@
 #[macro_use]
 extern crate log;
 
+use once_cell::sync::Lazy;
+
 pub use crate::languages::*;
-use std::{ffi::OsStr, path::Path};
+use std::{collections::HashMap, ffi::OsString, path::Path};
 
 pub mod languages;
 pub mod tree_sitter;
 
 pub fn guess_language(path: &Path) -> Option<&'static Language> {
-    for lang in LANGUAGES {
-        for ext in lang.filenames {
-            if path.file_name() == Some(OsStr::new(ext)) {
-                return Some(lang);
+    static FILE_NAMES: Lazy<HashMap<OsString, &'static Language>> = Lazy::new(|| {
+        let mut file_names = HashMap::new();
+        for language in LANGUAGES.iter() {
+            for file_name in language.filenames.iter() {
+                file_names.insert(file_name.into(), language);
             }
         }
+        file_names
+    });
 
-        for ext in lang.extensions {
-            if path.extension() == Some(OsStr::new(ext)) {
-                return Some(lang);
+    static EXTENSIONS: Lazy<HashMap<OsString, &'static Language>> = Lazy::new(|| {
+        let mut extensions = HashMap::new();
+        for language in LANGUAGES.iter() {
+            for extension in language.extensions.iter() {
+                extensions.insert(extension.into(), language);
             }
+        }
+        extensions
+    });
+
+    if let Some(file_name) = path.file_name() {
+        if let Some(language) = FILE_NAMES.get(file_name) {
+            return Some(language);
+        }
+    }
+
+    if let Some(extension) = path.extension() {
+        if let Some(language) = EXTENSIONS.get(extension) {
+            return Some(language);
         }
     }
 
