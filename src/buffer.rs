8a13b299043e746d547bfc46632b77a0ccfab47a
Seiya Nuta <nuta@seiya.me>
2025-12-15 21:17:44 +0900

    Update

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,6 +1,10 @@
+use std::cmp::{Ordering, max, min};
+
 use ropey::Rope;
 
-struct Position {
+/// The zero-based position in the buffer.
+#[derive(PartialEq, Eq, Ord, Copy, Clone)]
+pub struct Position {
     line: usize,
     column: usize,
 }
@@ -11,14 +15,65 @@ impl Position {
     }
 }
 
-pub struct Cursor {
-    pos: Position,
+impl PartialOrd for Position {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if self.line < other.line {
+            Some(Ordering::Less)
+        } else if self.line > other.line {
+            Some(Ordering::Greater)
+        } else {
+            self.column.partial_cmp(&other.column)
+        }
+    }
 }
 
-impl Cursor {
-    fn new(pos: Position) -> Self {
-        Self { pos }
+pub struct Range(Position, Position);
+
+impl Range {
+    fn new(start: Position, end: Position) -> Self {
+        Self(start, end)
     }
+
+    fn front(&self) -> Position {
+        min(self.0, self.1)
+    }
+
+    fn back(&self) -> Position {
+        max(self.0, self.1)
+    }
+
+    /// The position of the cursor after replacing this range of
+    /// text with `new_text`.
+    fn cursor_after_edit(&self, new_text: &str) -> Position {
+        let pos = self.front();
+        let num_newlines_added = new_text.matches('\n').count();
+        let num_newlines_deleted = self.back().line - self.front().line;
+
+        let y_diff = num_newlines_added.saturating_sub(num_newlines_deleted);
+
+        let mut x_diff = 0;
+        for c in new_text.chars() {
+            if c == '\n' {
+                x_diff = 0;
+            } else {
+                x_diff += 1;
+            }
+        }
+
+        let new_y = pos.line + y_diff;
+        let new_x = if new_text.contains('\n') {
+            x_diff
+        } else {
+            pos.column + x_diff
+        };
+
+        Position::new(new_y, new_x)
+    }
+}
+
+pub enum Cursor {
+    Normal(Position),
+    Selection(Range),
 }
 
 pub struct Buffer {
@@ -30,14 +85,12 @@ impl Buffer {
     pub fn new() -> Self {
         Self {
             rope: Rope::new(),
-            cursors: vec![Cursor::new(Position::new(0, 0))],
+            cursors: vec![Cursor::Normal(Position::new(0, 0))],
         }
     }
 
     pub fn insert_str(&mut self, text: &str) {
-        for c in self.cursors.iter_mut().rev() {
-            // TODO:
-        }
+        for i in (0..self.cursors.len()).rev() {}
     }
 }
 
--- src/status_line.rs
+++ src/status_line.rs
@@ -1,7 +1,6 @@
-use crate::terminal::{Frame, Widget, Color};
+use crate::terminal::{Color, Frame, Widget};
 
-pub struct StatusLine {
-}
+pub struct StatusLine {}
 
 impl StatusLine {
     pub fn new() -> Self {
@@ -13,6 +12,5 @@ impl Widget for StatusLine {
     fn render(&self, frame: &mut Frame) {
         let y = frame.height.saturating_sub(1);
         frame.fill_reversed(y, 0, frame.width);
-        frame.draw_str(y, 0, " noa");
     }
 }
