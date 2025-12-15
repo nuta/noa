95cf9580f66a6311104fa61ee3ad208a49c17fa5
Seiya Nuta <nuta@seiya.me>
2025-12-15 18:42:18 +0900

    Update

--- src/logger.rs
+++ src/logger.rs
@@ -5,6 +5,9 @@ use std::sync::Mutex;
 
 use log::{LevelFilter, Log, Metadata, Record};
 
+const MAX_LOG_FILE_SIZE: u64 = 1024 * 1024 * 10;
+
+/// A file-based logger.
 pub struct Logger {
     file: Mutex<File>,
 }
@@ -23,7 +26,7 @@ impl Log for Logger {
     fn log(&self, record: &Record) {
         if self.enabled(record.metadata()) {
             let mut file = self.file.lock().unwrap();
-            let _ = writeln!(&mut file, "[{}] {}", record.level(), record.args());
+            let _ = writeln!(&mut file, "{}", record.args());
         }
     }
 
@@ -37,6 +40,12 @@ pub fn init() -> Result<(), Box<dyn std::error::Error>> {
     let home_dir = std::env::home_dir().expect("failed to get home directory");
     let log_path = home_dir.join(".noa.log");
     let file = OpenOptions::new().create(true).append(true).open(log_path).expect("failed to open log file");
+
+    // If the log file istoo big, truncate it.
+    if file.metadata().unwrap().len() > MAX_LOG_FILE_SIZE {
+        let _ = file.set_len(0);
+    }
+
     let logger = Logger::new(file);
     log::set_boxed_logger(Box::new(logger)).expect("failed to set logger");
     log::set_max_level(LevelFilter::Trace);
